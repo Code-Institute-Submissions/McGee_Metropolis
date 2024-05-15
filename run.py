2a4221280c249ef5482fd3eb964d7d96117df138
Sophie McGee <sophiebmcgee@outlook.com>
2024-05-15 13:43:10 +0000

    Define ANSI colour codes in a class

--- run.py
+++ run.py
@@ -16,6 +16,17 @@ ZONE_SYMBOLS = {
 '-': '⚪' # Empty space
 }
 
+# ANSI colour Codes for console colour
+class Color:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 #Google Sheets setup
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
